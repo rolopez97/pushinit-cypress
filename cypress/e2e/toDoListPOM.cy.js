c61fcc87ccc288a309e0c26aa77249b17d462fda
rolopez97 <rosarioblopez97@gmail.com>
2025-09-11 20:27:24 -0300

    fixture

--- cypress/e2e/toDoListPOM.cy.js
+++ cypress/e2e/toDoListPOM.cy.js
@@ -2,30 +2,31 @@ import toDoListPage from "../pages/toDoListPage";
 
 describe("To do list", () => {
     beforeEach(function (){
+        cy.fixture('toDoData').as('data');
         cy.visit("https://www.pushing-it.vercel.app/");
         cy.typeLogin(Cypress.env('PUSHING_IT_USER'), Cypress.env('PUSHING_IT_PASS'))
         cy.url().should("eq", "https://pushing-it.vercel.app/home")
     })
 
-    it("opens the to do list after login", () => {
+    it("opens the to do list after login", function () {
         toDoListPage.clickToDoListLink();
         cy.url().should("eq", "https://pushing-it.vercel.app/home/todolist")
     })
 
-    it("adds a to do item", () => {
+    it("adds a to do item", function () {
         toDoListPage.clickToDoListLink();
-        toDoListPage.addTask("First to do item")
-        toDoListPage.verifyTaskExists("First to do item")        
+        toDoListPage.addTask(this.data.taskName)
+        toDoListPage.verifyTaskExists(this.data.taskName)        
     })
 
-    it("checks completed tasks", () =>{
+    it("checks completed tasks", function (){
         toDoListPage.clickToDoListLink();
-        toDoListPage.completeTask("First to do item")
-        toDoListPage.verifyTaskExists("First to do item")
+        toDoListPage.completeTask(this.data.taskName)
+        toDoListPage.verifyTaskExists(this.data.taskName)
     })
 
-    it("deletes a to do item", () => {
+    it("deletes a to do item", function () {
         toDoListPage.clickToDoListLink();
-        toDoListPage.deletTask("First to do item");
+        toDoListPage.deletTask(this.data.taskName);
     })
 })
